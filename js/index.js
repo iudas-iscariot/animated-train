4ad977540fed64179b4bd4a227590944c271ea8f
unknown <uurfgdh@gmail.com>
2020-04-13 09:13:05 +0300

    name

--- js/index.js
+++ js/index.js
@@ -21,7 +21,7 @@ $(document).ready(function(){
     {
       breakpoint: 1024,
       settings: {
-          slidesToShow: 1,
+              slidesToShow: 1,
       }
     },
     {
@@ -44,7 +44,7 @@ $(document).ready(function(){
   slidesToShow: 3,
   slidesToScroll: 1,
   centerMode: true,
-
+  centerPadding: '0px',
 
 
         responsive: [
@@ -53,6 +53,8 @@ $(document).ready(function(){
       settings: {
         slidesToShow: 2,
         slidesToScroll: 1,
+        centerMode: true,
+        centerPadding: '0px',
         infinite: true,
         dots: false
       }
